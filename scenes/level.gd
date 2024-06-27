5a888ba7a170b6ecad97ab148b0605cbb203a261
AbdulqadirQ <a.q.r@hotmail.co.uk>
2024-06-27 17:17:06 +0100

    Draw laser on input

--- scenes/level.gd
+++ scenes/level.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 
 var meteor_scene: PackedScene = load("res://scenes/meteor.tscn")
+var laser_scene: PackedScene = load("res://scenes/laser.tscn")
 
 
 func _on_meteor_timer_timeout():
@@ -10,5 +11,6 @@ func _on_meteor_timer_timeout():
 
 
 func _on_player_laser(pos):
-	print("shoot laser")
-	print(pos)
+	var laser = laser_scene.instantiate()
+	$Lasers.add_child(laser)
+	laser.position = pos
