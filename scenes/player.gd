546287762d48dc82c18afdeff58491d44915548d
AbdulqadirQ <a.q.r@hotmail.co.uk>
2024-06-27 14:11:16 +0100

    Add other borders + meteor collision

--- scenes/player.gd
+++ scenes/player.gd
@@ -4,7 +4,7 @@ extends CharacterBody2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	position = Vector2(100,500)
+	position = Vector2(640,360)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
