3b1a6790d0b48e6d8d3d88700215df9be1ac3947
AbdulqadirQ <a.q.r@hotmail.co.uk>
2024-06-27 20:37:30 +0100

    Added star animations + 10 of them on level randomly

--- scenes/level.gd
+++ scenes/level.gd
@@ -14,3 +14,15 @@ func _on_player_laser(pos):
 	var laser = laser_scene.instantiate()
 	$Lasers.add_child(laser)
 	laser.position = pos
+
+func _ready():
+	var size := get_viewport().get_visible_rect().size
+	var rng := RandomNumberGenerator.new()
+	for star in $Stars.get_children():
+		var random_x = rng.randi_range(0, size.x)
+		var random_y = rng.randi_range(0, size.y)
+		star.position = Vector2(random_x, random_y)
+	
+
+func _on_star_destroy_timer_timeout():
+	pass # Replace with function body.
