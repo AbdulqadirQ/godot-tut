e8dfc4355e1d84e392198e9212dcebbfc1724c91
AbdulqadirQ <a.q.r@hotmail.co.uk>
2024-06-27 01:17:16 +0100

    Add rotation specifically to node

--- scenes/player.gd
+++ scenes/player.gd
@@ -9,3 +9,4 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	position += Vector2(1,0) * 10 * delta
+	$PlayerImage.rotation += 0.1 * delta
