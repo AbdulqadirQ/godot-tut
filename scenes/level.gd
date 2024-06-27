7c6e66924383696b3059272c0c89bc4c727a3381
AbdulqadirQ <a.q.r@hotmail.co.uk>
2024-06-27 14:55:26 +0100

    Meteor spawn timer

--- scenes/level.gd
+++ scenes/level.gd
@@ -1,7 +1,8 @@
 extends Node2D
 
 
+var meteor_scene: PackedScene = load("res://scenes/meteor.tscn")
 
-
-func _on_timer_timeout():
-	print("meteor")
+func _on_meteor_timer_timeout():
+	var meteor = meteor_scene.instantiate()
+	$Meteors.add_child(meteor)
