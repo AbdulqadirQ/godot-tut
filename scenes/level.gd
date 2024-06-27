c87dc44e2d472b3a15e379e60415c3f537a26a05
AbdulqadirQ <a.q.r@hotmail.co.uk>
2024-06-27 17:14:39 +0100

    Add custom scene for triggering input of player in level

--- scenes/level.gd
+++ scenes/level.gd
@@ -3,6 +3,12 @@ extends Node2D
 
 var meteor_scene: PackedScene = load("res://scenes/meteor.tscn")
 
+
 func _on_meteor_timer_timeout():
 	var meteor = meteor_scene.instantiate()
 	$Meteors.add_child(meteor)
+
+
+func _on_player_laser(pos):
+	print("shoot laser")
+	print(pos)
--- scenes/player.gd
+++ scenes/player.gd
@@ -2,6 +2,8 @@ extends CharacterBody2D
 
 @export var speed := 500
 
+signal laser(pos)
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	position = Vector2(640,360)
@@ -12,3 +14,6 @@ func _process(delta):
 	var direction = Input.get_vector("left","right","up","down")
 	velocity = direction * speed
 	move_and_slide()
+	
+	if Input.is_action_just_pressed("shoot"):
+		laser.emit(position)
