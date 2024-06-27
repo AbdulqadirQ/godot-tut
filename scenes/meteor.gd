1de2032bb568331a6d3bb4e0e6be6ca1b1e43f1e
AbdulqadirQ <a.q.r@hotmail.co.uk>
2024-06-27 15:50:35 +0100

    Add rand speed, rotation and direction to meteor

--- scenes/meteor.gd
+++ scenes/meteor.gd
@@ -1,5 +1,10 @@
 extends Area2D
 
+var speed: int
+var rotation_speed: int
+var direction_x: float
+var direction_y: float
+
 func _ready():
 	var rng := RandomNumberGenerator.new()
 	
@@ -8,8 +13,15 @@ func _ready():
 	var random_y = rng.randi_range(-150, -50)
 	position = Vector2(random_x, random_y)
 	
+	speed = rng.randf_range(300, 500)
+	direction_x = rng.randf_range(-1, 1)
+	direction_y = rng.randf_range(0.1, 1)
+	rotation_speed = rng.randf_range(40, 100)
+
 func _process(delta):
-	position += Vector2(0, 1.0) * 400 * delta
+	position += Vector2(direction_x, direction_y) * speed * delta
+	rotation_degrees += rotation_speed * delta
 
 func _on_body_entered(body):
 	print("Body collision: ", body)
+
